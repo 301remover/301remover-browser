bb17eaf71eb9e0afe598d1424b89aecc67368304
Campos Botello <camposbotellob@ad.msoe.edu>
2019-02-23 15:14:41 -0600

    Changed popup styling

--- popup.js
+++ popup.js
@@ -3,12 +3,3 @@ let counter = document.getElementById('counter');
 chrome.storage.sync.get('counter', function(data) {
   counter.innerHTML = "Resolved urls: " + data.counter;
 });
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {file: 'script.js'});
-    });
-  };
