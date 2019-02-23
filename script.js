838cc046ef73b4d51082070d5d887df7a95a40dc
Sean Lang <slang800@gmail.com>
2019-02-23 14:52:59 -0600

    fix case where href is null

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let tinyLinks = []
 const shorter = /^https?:\/\/(bit\.ly|tinyurl\.com|ow\.ly|snipurl\.com)\/[0-9a-zA-Z]+$/
 
 for (let i = 0; i < links.length; i++) {
-  if (links[i].getAttribute('href').match(shorter)) {
+  const href = links[i].getAttribute('href')
+  if (href != null && href.match(shorter)) {
     let newURL = new URL(links[i].getAttribute('href'))
     tagsMatch.push(links[i])
     tinyLinks.push(newURL.hostname + newURL.pathname)
