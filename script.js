875f630558beb644edd0eb345cb38979eb4ba93e
Ben Giese <gieseba@msoe.edu>
2019-10-23 16:04:05 -0500

    Not needed as we won't have a server operating to get shortend urls yet

--- script.js
+++ script.js
@@ -16,31 +16,3 @@ for (let i = 0; i < links.length; i++) {
 
 console.log(tinyLinks)
 console.log(tagsMatch)
-
-let res = makeRequest(tinyLinks, tagsMatch)
-
-function makeRequest (links, tags) {
-  let serverURL = 'http://192.168.56.133:5000/api/bulk_resolve'
-  let requestJson = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(links)
-  }
-  fetch(serverURL, requestJson).then((res) => {
-    return res.json()
-  }).then(data => {
-    let resolved = 0
-    for (let i = 0; i < data.length; i++) {
-      if (data[i] !== null) {
-        ++resolved
-        tags[i].setAttribute('href', data[i])
-      }
-    }
-    chrome.storage.sync.set({'counter': resolved})
-  }).catch((res) => {
-    return null
-  })
-}
