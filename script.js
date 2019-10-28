e709ffd21acd57c56c7022eb7cefc7d6c654e08a
Ben Giese <gieseba@msoe.edu>
2019-10-28 15:29:51 -0500

    Add temporary shortened link replacements

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-alert("Hello from your Chrome extension! v1.1")
-
 var links = document.getElementsByTagName('a')
 var tagsMatch = []
 let tinyLinks = []
@@ -12,6 +10,20 @@ for (let i = 0; i < links.length; i++) {
     tagsMatch.push(links[i])
     tinyLinks.push(newURL.hostname + newURL.pathname)
   }
+}
+
+let res = makeRequest(tinyLinks, tagsMatch)
+
+function makeRequest(links, tags) {
+  var data = ['https://www.google.com', 'https://github.com']
+  let resolved = 0
+  for(let i = 0; i < data.length; i++) {
+    if(data[i] !== null) {
+      ++resolved
+      tags[i].setAttribute('href', data[i])
+    }
+  }
+  chrome.storage.sync.set({'counter': resolved})
 }
 
 console.log(tinyLinks)
