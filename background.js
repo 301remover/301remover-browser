5a05f541d9d523592eead6ff94ed59c5d9bcd94e
Ben Giese <gieseba@msoe.edu>
2019-11-09 15:37:04 -0600

    Request successfully retrieved

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function() {
+  chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log('The color is green.');
     });
@@ -12,9 +12,62 @@ chrome.runtime.onInstalled.addListener(function() {
       }]);
     });
   });
-chrome.runtime
-/*
-    "browser_action": {
-      "default_icon": "icon.png"
-    },
-*/
+
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+    console.log('before request');
+    if(details.type === 'main_frame') {
+      if(details.url.includes('bit.ly')) {
+        console.log('ALERT BITLY DETECTED');
+        return { redirectUrl: "http://www.google.com" }; 
+      }
+    }
+  },
+    {urls: ["<all_urls>"]}, ["blocking"]
+  );
+
+  chrome.webRequest.onCompleted.addListener(
+    function(details) {
+      const { tabId, requestId } = details;
+      if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+          return;
+      }
+
+      const request = tabStorage[tabId].requests[requestId];
+
+      Object.assign(request, {
+          endTime: details.timeStamp,
+          requestDuration: details.timeStamp - request.startTime,
+          status: 'complete'
+      });
+      console.log(tabStorage[tabId].requests[details.requestId]);
+  },
+    {urls: ["<all_urls>"]}
+  );
+
+  chrome.webRequest.onErrorOccurred.addListener(
+    function(details) {
+      //console.log('Web Request Error Occurred');
+      //console.log(details);
+      const { tabId, requestId } = details;
+      if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+          return;
+      }
+  },
+    {urls: ["<all_urls>"]}
+  );
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  fetch(request.input, request.init).then(function(response) {
+    return response.text().then(function(text) {
+      sendResponse([{
+        body: text,
+        status: response.status,
+        statusText: response.statusText,
+      }, null]);
+    });
+  }, function(error) {
+    sendResponse([null, error]);
+  });
+  return true;
+});
--- script.js
+++ script.js
@@ -5,23 +5,41 @@ const shorter = /^https?:\/\/(bit\.ly|tinyurl\.com|ow\.ly|snipurl\.com)\/[0-9a-z
 
 let shorteners = []
 
+function fetchResource(input, init) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage({input, init}, messageResponse => {
+      const [response, error] = messageResponse;
+      if (response === null) {
+        reject(error);
+      } else {
+        // Use undefined on a 204 - No Content
+        const body = response.body ? new Blob([response.body]) : undefined;
+        resolve(new Response(body, {
+          status: response.status,
+          statusText: response.statusText,
+        }));
+      }
+    });
+  });
+}
+
 function getShorteners() {
-  let serverUrl = ''
+  let serverUrl = 'http://192.168.1.140:4000/api/shorteners'
   let requestJson = {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     }
   }
-  fetch(serverUrl, requestJson).then((res) => {
+  fetchResource(serverUrl, requestJson).then((res) => {
     console.log(res);
-    console.log(res.json());
-    return res.json();
+    return res.text();
   }).then(data => {
     console.log('data: ' + data);
   })
 }
+
 
 getShorteners(); //got shorteners
 
@@ -32,6 +50,20 @@ for (let i = 0; i < links.length; i++) {
     tagsMatch.push(links[i])
     tinyLinks.push(newURL.hostname + newURL.pathname)
   }
+
+  /*
+  //can't be a contains, has to be startsWith???
+  //but then we have to loop over the whole list
+  if (href != null) {
+    shorteners.forEach(function(element) {
+      console.log(href + ' *** ' + element);
+      if(href.startsWith(element)) {
+        let newURL = new URL(links[i].getAttribute('href'));
+        tagsMatch.push(links[i]);
+        tinyLinks.push(newURL.hostname + newURL.pathname);
+      }
+  });
+  */
 }
 
 let res = makeRequest(tinyLinks, tagsMatch)
