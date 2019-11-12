b3c8bdb40578f9369dd361cabb06dc9cccc8df05
Ben Giese <gieseba@msoe.edu>
2019-11-12 16:20:35 -0600

    Remove unfinished address bar functionality

--- background.js
+++ background.js
@@ -27,7 +27,6 @@ getShorteners = () => {
       
 chrome.runtime.onStartup.addListener(function () {
   getShorteners().then((resp) => {
-    console.log('Startup hit')
     console.log(shortenerRegex)
     shortenerRegex = resp
   })
@@ -51,63 +50,16 @@ chrome.runtime.onInstalled.addListener(function () {
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
-    console.log('before request');
     if (details.type !== 'main_frame' || !masterRegex.test(details.url)) {
-      return 
+      return
     }
-    console.log('hit')
-    let domainMatch = details.url.match(masterRegex)
-    let match = details.url.match(shortenerRegex[domainMatch[2]])
-
-    console.log(domainMatch)
-    console.log(match)
-
-    let link = {
-      domain: domainMatch[2],
-      shortcode: match[1]
-    }
-    console.log(link)
-
-    let respFromServer = resolveURLs(link)
-
-    respFromServer.then(function (value) {
-      console.log(value);
-      return {
-        redirectUrl: value
-      };
-    })
+    return { cancel: true} 
   }, {
-    urls: ["<all_urls>"]
-  }, ["blocking"]
-);
-
-// chrome.webRequest.onCompleted.addListener(
-//   function (details) {
-//     const {
-//       tabId,
-//       requestId
-//     } = details;
-//     if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
-//       return;
-//     }
-
-//     const request = tabStorage[tabId].requests[requestId];
-
-//     Object.assign(request, {
-//       endTime: details.timeStamp,
-//       requestDuration: details.timeStamp - request.startTime,
-//       status: 'complete'
-//     });
-//     console.log(tabStorage[tabId].requests[details.requestId]);
-//   }, {
-//     urls: ["<all_urls>"]
-//   }
-// );
+    urls: ["<all_urls>"]}, ["blocking"]
+)
 
 chrome.webRequest.onErrorOccurred.addListener(
   function (details) {
-    //console.log('Web Request Error Occurred');
-    //console.log(details);
     const {
       tabId,
       requestId
@@ -134,37 +86,3 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   });
   return true;
 });
-
-
-//TODO chrome.runtime.onStartup to get the master regex from server
-
-
-function resolveURLs(link) {
-  console.log(link)
-  const requestLink = [{
-    domain: link.domain,
-    shortcode: link.shortcode
-  }]
-  console.log(JSON.stringify({
-    shortlinks: requestLink
-  }))
-  const requestJson = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      shortlinks: requestLink
-    })
-  }
-  return fetch(resolverURL, requestJson).then((res) => {
-    return res.json()
-  }).then(resp => {
-    console.log(resp)
-    return resp[0]
-  }).catch((res) => {
-    console.log("ERROR: " + res);
-    return null
-  })
-}
