9af1441e4b7b40aed1baf04fc4a9987481a12e56
Ben Giese <gieseba@msoe.edu>
2019-11-20 20:26:23 +0000

    Block known shorteners  & resolves shortened services links on page.

--- script.js
+++ script.js
@@ -1,53 +1,121 @@
-var links = document.getElementsByTagName('a')
+/* global chrome, Blob, Response */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "_" }] */
+
 var tagsMatch = []
-let tinyLinks = []
-const shorter = /^https?:\/\/(bit\.ly|tinyurl\.com|ow\.ly|snipurl\.com)\/[0-9a-zA-Z]+$/
+const tinyLinks = []
 
-let shorteners = []
+const masterRegex = /^http(s?):\/\/(bit\.ly|tinyurl\.com|goo\.gl)/
+const shortenersURL = 'http://301r.dev/api/shorteners'
+const resolverURL = 'http://301r.dev/api/unshorten'
 
-function getShorteners() {
-  let serverUrl = ''
-  let requestJson = {
+const getShorteners = () => {
+  const requestJson = {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     }
   }
-  fetch(serverUrl, requestJson).then((res) => {
-    console.log(res);
-    console.log(res.json());
-    return res.json();
-  }).then(data => {
-    console.log('data: ' + data);
+  return fetchResource(shortenersURL, requestJson).then((res) => {
+    return res.json()
+  }).then(resp => {
+    const shortenerRegex = {}
+    for (const key in resp.data) {
+      const obj = (resp.data[key])
+      shortenerRegex[obj.domain] = new RegExp(obj.url_pattern.replace('{shortcode}', '([' + obj.shortcode_alphabet + ']+)'))
+    }
+    return shortenerRegex
   })
 }
 
-getShorteners(); //got shorteners
-
-for (let i = 0; i < links.length; i++) {
-  const href = links[i].getAttribute('href')
-  if (href != null && href.match(shorter)) { //TODO change to check using contains and shorteners
-    let newURL = new URL(links[i].getAttribute('href'))
-    tagsMatch.push(links[i])
-    tinyLinks.push(newURL.hostname + newURL.pathname)
-  }
-}
-
-let res = makeRequest(tinyLinks, tagsMatch)
-
-function makeRequest(links, tags) {
-  var data = ['https://www.google.com', 'https://archive.org']
-  let resolved = 0
-  for(let i = 0; i < data.length; i++) {
-    if(data[i] !== null) {
-      ++resolved
-      tags[i].setAttribute('href', data[i])
+getShorteners().then((shortenerRegex) => {
+  const nodes = document.getElementsByTagName('a')
+  const links = Array.from(nodes).map((node) =>
+    ({
+      href: node.getAttribute('href'),
+      node: node
+    })
+  ).filter(({ href }) =>
+    href != null
+  ).map(({ href, node }) => {
+    return {
+      href: href,
+      node: node,
+      match: href.match(masterRegex)
     }
-  }
-  chrome.storage.sync.set({'counter': resolved})
+  }).filter((link) => {
+    return link.match != null
+  }).map((link) => {
+    const [_, httpsMatch, domain] = link.match
+    const shortenerRegexMatch = link.href.match(shortenerRegex[domain])
+    return {
+      href: link.href,
+      node: link.node,
+      domain: domain,
+      shortcode: shortenerRegexMatch == null ? null : shortenerRegexMatch[1],
+      isHttps: httpsMatch !== ''
+    }
+  }).filter((link) =>
+    link.shortcode != null
+  ).map((link) => {
+    tagsMatch.push(link.node)
+    const newURL = new URL(link.href)
+    tinyLinks.push(newURL.hostname + newURL.pathname)
+    return link
+  })
+
+  resolveURLs(links, tagsMatch)
+})
+
+function fetchResource (input, init) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage({
+      input,
+      init
+    }, messageResponse => {
+      const [response, error] = messageResponse
+      if (response === null) {
+        reject(error)
+      } else {
+        const body = response.body ? new Blob([response.body]) : undefined
+        resolve(new Response(body, {
+          status: response.status,
+          statusText: response.statusText
+        }))
+      }
+    })
+  })
 }
 
-
-console.log(tinyLinks)
-console.log(tagsMatch)
+function resolveURLs (links, tags) {
+  const requestLinks = links.map((link) =>
+    ({
+      domain: link.domain,
+      shortcode: link.shortcode
+    })
+  )
+  const requestJson = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      shortlinks: requestLinks
+    })
+  }
+  return fetchResource(resolverURL, requestJson).then((res) => {
+    return res.json()
+  }).then(resp => {
+    let resolved = 0
+    for (let i = 0; i < resp.length; i++) {
+      if (resp[i] !== null) {
+        ++resolved
+        tags[i].setAttribute('href', resp[i])
+      }
+    }
+    chrome.storage.sync.set({
+      counter: resolved
+    })
+  })
+}
