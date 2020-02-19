7ae47ad5378565300d56a3714a5ffc6c80f1aebe
Sean Lang <slang800@gmail.com>
2020-02-19 11:43:10 -0600

    remove popup feature

--- popup.js
+++
@@ -1,7 +0,0 @@
-/* global chrome */
-
-const counter = document.getElementById('counter')
-
-chrome.storage.sync.get('counter', function (data) {
-  counter.innerHTML = 'Resolved urls: ' + data.counter
-})
